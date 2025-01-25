475d3ac6b8de21a5ac149887dfaad9f53cd603b8
sezid <bradsajid@gmail.com>
2025-01-25 22:27:41 +0600

    updated picture formats

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,25 +1,23 @@
-import plus from './plus.png'
-import github from './github.png'
-import phone from './phone.png'
-import location from './location.png'
-import linkedin from './linkedin.png'
-import email from './email.png'
-import web from './web.png'
-import headshot from './headshot.png'
-import facebook from './facebook.png'
-import project_1 from './project_1.png'
-import cert_1 from './cert-1.png'
-import cert from './certificate.png'
-import cert_2 from './cert-2.png'
-import project_2 from './project_2.png'
-import menu_icon from './menu_icon.png'
-import dropdown_icon from './dropdown_icon.png'
-import background from './background.webp'
-import arrow from './right-arrow.png'
+import plus from "./plus.webp";
+import github from "./github.webp";
+import phone from "./phone.webp";
+import location from "./location.webp";
+import linkedin from "./linkedin.webp";
+import email from "./email.webp";
+import web from "./web.webp";
+import headshot from "./headshot.webp";
+import facebook from "./facebook.webp";
+import project_1 from "./project_1.webp";
+import cert_1 from "./cert-1.webp";
+import cert from "./certificate.webp";
+import cert_2 from "./cert-2.webp";
+import project_2 from "./project_2.webp";
+import menu_icon from "./menu_icon.webp";
+import dropdown_icon from "./dropdown_icon.webp";
+import background from "./background.webp";
+import arrow from "./right-arrow.webp";
 
-
-
-export const assets ={
+export const assets = {
   plus,
   github,
   email,
@@ -37,58 +35,59 @@ export const assets ={
   menu_icon,
   dropdown_icon,
   background,
-  arrow
-}
-
+  arrow,
+};
 
 export const projects = [
   {
-      name:'Khidaaa',
-      image: project_1,
-      summary: "A food delivery website made using React only. It contains little features such as adding items to cart and filtering category of foods amongst others. Frontend part is completed, but more features to come in the future.",
-      github:"https://github.com/sezid/khidaaa",
-      link:"https://khidaaa.netlify.app"
-    },
+    name: "Khidaaa",
+    image: project_1,
+    summary:
+      "A food delivery website made using React only. It contains little features such as adding items to cart and filtering category of foods amongst others. Frontend part is completed, but more features to come in the future.",
+    github: "https://github.com/sezid/khidaaa",
+    link: "https://khidaaa.netlify.app",
+  },
   {
-    name:"Dapper",
-    image:project_2,
-    summary:"An ecommerce clothing store from built using react and tailwind CSS. The project is still incomplete however, expecting to add some features soon.",
-    github:"https://github.com/sezid/e-commerce",
-    link:"https://dappar.netlify.app"
-  }  
-]
+    name: "Dapper",
+    image: project_2,
+    summary:
+      "An ecommerce clothing store from built using react and tailwind CSS. The project is still incomplete however, expecting to add some features soon.",
+    github: "https://github.com/sezid/e-commerce",
+    link: "https://dappar.netlify.app",
+  },
+];
 
-export const news=[
+export const news = [
   {
-    number:1,
-    date:'13/08/2018',
-    title:"O Levels Completed"
+    number: 1,
+    date: "13/08/2018",
+    title: "O Levels Completed",
   },
 
   {
-    number:2,
-    date:'15/08/2020',
-    title:"A Levels Completed"
+    number: 2,
+    date: "15/08/2020",
+    title: "A Levels Completed",
   },
   {
-    number:3,
-    date:'28/09/2020',
-    title:"Started Studying Computer Science and Engineering at BRAC University"
+    number: 3,
+    date: "28/09/2020",
+    title:
+      "Started Studying Computer Science and Engineering at BRAC University",
   },
   {
-    number:4,
-    date:'01/10/2022',
-    title:"Changed Undergraduate Program from CSE to CS"
+    number: 4,
+    date: "01/10/2022",
+    title: "Changed Undergraduate Program from CSE to CS",
   },
   {
-    number:5,
-    date:'01/10/2024',
-    title:"Graduated from BRAC University"
+    number: 5,
+    date: "01/10/2024",
+    title: "Graduated from BRAC University",
   },
-  { number:6,
-    date:'05/01/2025',
-    title:'Started working in LIIA Smart Inc.'
-  }
-
-]
-
+  {
+    number: 6,
+    date: "05/01/2025",
+    title: "Started working in LIIA Smart Inc.",
+  },
+];
